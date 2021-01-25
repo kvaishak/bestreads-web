f3a35d1818b082e8ea62bda4b6ce35f9130c5ad0
kvaishak <kvaishak47@gmail.com>
2021-01-25 21:45:14 +0530

    some changes stat

--- src/App.js
+++ src/App.js
@@ -72,14 +72,14 @@ function App() {
                                     Name</label>
                                 <input type="text" name="" id="" placeholder="Your User Name "
                                     className="w-full px-4 py-2 mt-2 text-base bg-gray-100 border-transparent rounded-lg ext-blue-700 focus:border-gray-500"
-                                    autoFocus autoComplete required />
+                                    autoFocus autoComplete="true" required />
                             </div>
                             <div className="mt-4">
                                 <label className="block text-base font-medium leading-relaxed text-gray-700">Email
                                     Address</label>
                                 <input type="email" name="" id="" placeholder="Your Email "
                                     className="w-full px-4 py-2 mt-2 text-base bg-gray-100 border-transparent rounded-lg ext-blue-700 focus:border-gray-500"
-                                    autoFocus autoComplete required />
+                                    autoFocus autoComplete="true" required />
                             </div>
                             <div className="flex flex-wrap mt-4 mb-6 -mx-3">
                                 <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
@@ -104,7 +104,7 @@ function App() {
                             </div>
                             <button type="submit" className="block w-full px-4 py-3 mt-6 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg bg-gradient-to-r from-blue-700 hover:from-blue-500 to-blue-500 hover:to-blue-700 focus:shadow-outline focus:outline-none">Log In</button>
                         </form>
-                        <p className="mt-8 text-center">Already have an account? <a href="#"
+                        <p className="mt-8 text-center">Already have an account? <a href="https://www.google.com/"
                                 className="font-semibold text-blue-500 hover:text-blue-700">Sign
                                 In</a></p>
                     </div>
