3491d9e765becbde2f1394ec099e8c3089589c50
kvaishak <kvaishak47@gmail.com>
2021-01-31 13:20:24 +0530

    Added a simple dashboard page - needs reworking

--- src/components/ProfileUI/ProfileUI.js
+++ src/components/ProfileUI/ProfileUI.js
@@ -12,7 +12,7 @@ function ProfileUI(props) {
                 className="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
                 <h2 className="mb-1 text-xs font-medium tracking-widest text-blue-500 title-font">Welcome</h2>
                 <h1 className="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-800 lg:text-left lg:text-5xl title-font">
-                    {props.userEmail}
+                    {props.userName}
                 </h1>
                 <div className="flex justify-center">
                     <p className="mb-8 text-base leading-relaxed text-center text-gray-700 lg:text-left lg:text-1xl">
