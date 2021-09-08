3ef0f24d0e2a39b842144102a7aa6135231b9e14
kvaishak <kvaishak47@gmail.com>
2021-09-08 20:02:31 +0530

    Added Loader

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import Layout from '../Layout/Layout'
-import CurrentSectionUI from '../../components/CurrentSectionUI/CurrentSectionUI';
+import Layout from "../Layout/Layout";
+import CurrentSectionUI from "../../components/CurrentSectionUI/CurrentSectionUI";
 
-import { useBooks, bookStatus } from '../../hooks/useBooks'
-import FinishedSectionUI from '../../components/FinishedSectionUI/FinishedSectionUI';
-import ToReadSectionUI from '../../components/ToReadSectionUI/ToReadSectionUI';
-
+import { useBooks, bookStatus } from "../../hooks/useBooks";
+import FinishedSectionUI from "../../components/FinishedSectionUI/FinishedSectionUI";
+import ToReadSectionUI from "../../components/ToReadSectionUI/ToReadSectionUI";
+import Loader from "../../utils/Loader/Loader";
 
 const Dashboard = () => {
+  const { data } = useBooks();
 
-    const { data } = useBooks();
+  console.log("hello there");
+  console.log(data.books);
+  var currentlyReadingBooks = [],
+    finishedReadingBooks = [],
+    toReadBooks = [];
 
-    console.log(data.books);
-    var currentlyReadingBooks = [], finishedReadingBooks = [], toReadBooks = [];
+  data.books.forEach((book) => {
+    switch (book.status) {
+      case bookStatus.currentlyReading:
+        currentlyReadingBooks.push(book);
+        break;
+      case bookStatus.finishedReading:
+        finishedReadingBooks.push(book);
+        break;
+      case bookStatus.toRead:
+        toReadBooks.push(book);
+        break;
+      default:
+        currentlyReadingBooks.push(book);
+    }
+  });
 
-    data.books.forEach((book) => {
-        switch(book.status){
-            case bookStatus.currentlyReading : currentlyReadingBooks.push(book);
-                                                break;
-            case bookStatus.finishedReading : finishedReadingBooks.push(book);
-                                                break;
-            case bookStatus.toRead : toReadBooks.push(book);
-                                                break;
-            default : currentlyReadingBooks.push(book);
-        }
-    })
+  if (data.books.length === 0) {
+    return (
+      <Layout>
+        <Loader />
+      </Layout>
+    );
+  }
 
-
-    return ( 
-        <Layout>
-            <CurrentSectionUI books={currentlyReadingBooks}/>
-            {finishedReadingBooks.length > 0 && <FinishedSectionUI books={finishedReadingBooks}/>}
-            {toReadBooks.length > 0 && <ToReadSectionUI books={toReadBooks}/>}
-        </Layout>
-     );
-}
+  return (
+    <Layout>
+      <CurrentSectionUI books={currentlyReadingBooks} />
+      {finishedReadingBooks.length > 0 && (
+        <FinishedSectionUI books={finishedReadingBooks} />
+      )}
+      {toReadBooks.length > 0 && <ToReadSectionUI books={toReadBooks} />}
+    </Layout>
+  );
+};
 
 export default Dashboard;
